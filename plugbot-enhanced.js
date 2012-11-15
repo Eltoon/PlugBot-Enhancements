5aeede9c33b55f628f4dad707ec2add23ca183c2
RainDasher <raindasher@anypony.com>
2012-11-14 16:25:27 -0800

    Update plugbot-enhanced.js

--- plugbot-enhanced.js
+++ plugbot-enhanced.js
@@ -30,7 +30,7 @@
  * NOTE:  This is all procedural as hell because prototypes and any 
  * 			OOP techniques in Javascript are stupid and confusing.
  * 
- * Original author 	Conner Davis ([VIP] ?Logïç®) 
+ * Original author 	Conner Davis ([VIP] ?Logï¿½ï¿½ï¿½) 
  * 			Harrison Schneidman ([VIDJ] EX?)
  *
  *
@@ -230,6 +230,28 @@ function appendUser(user) {
 }
 
 /*End of PlugBot Core - Colgate's Expansion past here*/
+// Volume mute
+if(window.navigator.vendor.match(/Google/)) { 
+  var div = document.createElement("div");
+  div.setAttribute("onclick", "return window;");
+  unsafeWindow = div.onclick();
+};
+
+function toggle(event){if (unsafeWindow.Playback.isMuted){
+		setTimeout(function(){unsafeWindow.Playback.onSoundButtonClick()},2000)
+	}
+	unsafeWindow.API.removeEventListener(unsafeWindow.API.DJ_ADVANCE,toggle)
+}
+
+setTimeout(function() {
+unsafeWindow.$('#volume').append("<div id='muteSong' style='position: absolute; top: 32px;border-style:solid; border-width:1px; padding: 1px; '> Mute current song</div>")
+unsafeWindow.$('#muteSong').click(function(){
+	unsafeWindow.Playback.onSoundButtonClick()
+
+	unsafeWindow.API.addEventListener(unsafeWindow.API.DJ_ADVANCE, toggle)
+	console.log('done')
+})
+},5000);
 
 /*AppendToChat*/
 function appendToChat(message, from, color, changeToColor){
@@ -332,3 +354,5 @@ initAPIListeners();
 populateUserlist();
 displayUI();
 initUIListeners();
+//background much needed
+GM_addStyle("html { background: #000 url(http://i.imgur.com/weYWp.jpg) no-repeat scroll center top; } #room-wheel { display: none !important; }");
